aa95f3ed8191cd702ec9269aac499783d9003029
drumslayert <ddc@terrymcalhoun.com>
2015-10-21 08:54:08 -0400

    add angular and sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,16 @@
 
-// alert("Im working");
+    $("ul.tabs li a.button").on("click",function(){
+      $(this).addClass("active").siblings().removeClass("active");
+      $($(this).attr("href")).addClass("active").siblings().removeClass("active");
 
-//http://api.jquery.com/jquery.getjson/
+    }); (jQuery.noConflict())
 
-//
-// $.getJSON( "octocat.json", function() {
-//   console.log( "success" );
-// })
-
-// $('#tabs').on('click', function() {
-//   var active_button = $('.tabs li a.active');
-// });
-//
-
-
-// $(document).ready(function(){
-//   $("li p").hide();
-//   $("li").css({"cursor":"pointer"});
-//   $(this).on('click',function(){
-//      $("p").slideToggle();
-//   });
-// });
-
-
-$("li p").hide();
-$('li').click(function(e){
-    if (e.target == this)
-    {
-     $(this).children().slideToggle();
-    }
+;(function(){
+  angular.module("Github", [ ])
+    .run(function($http, $rootScope){
+    $http.get('/apis/github/users/drumslayert/drumslayert.json')
+     .then(function(response){
+      $rootScope.user = response.data;
+     });
+    })
 });
-// $.getJSON( "octocat.json", function( json ) {
-//   console.log( "JSON Data: " + json.users[ 3 ].name );
-//  });
-//
-//
-// $.ajax('../api/github/users/octocat.json').then(function(data){
-//   console.log(arguments);
-// });

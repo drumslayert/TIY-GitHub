9ed6dcfd346ebf05d036a8f0553b220525e13ffc
drumslayert <ddc@terrymcalhoun.com>
2015-10-21 22:28:06 -0400

    add angular but no repeat yet :(

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,34 @@
 
     }); (jQuery.noConflict())
 
-;(function(){
-  angular.module("Github", [ ])
+;(function(){ //IIFE
+  angular.module('Github', [ ])
     .run(function($http, $rootScope){
+      // console.log("test bullet")
+      // $rootScope.login = "Test";
+
     $http.get('/apis/github/users/drumslayert/drumslayert.json')
      .then(function(response){
       $rootScope.user = response.data;
-     });
+
+
+      // {
+      //   login: response.data.login,
+      //   body: response.data.body,
+      // }
+    });
     })
-});
+
+    .run(function($http, $rootScope){
+      $http.get('/apis/github/users/drumslayert/issues.json')
+        .then(function(response){
+          // console.log(response.data[0].body);
+        $rootScope.comments = response.data[0];
+        // {
+        // login:  response.data.login,
+        // body: response.data.body,
+        // }
+
+        });
+    })
+})();// End IIFE
